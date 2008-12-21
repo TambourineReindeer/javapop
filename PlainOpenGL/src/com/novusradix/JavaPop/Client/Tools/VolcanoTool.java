964130bd11ea71a9cef973a8e937dee49b6ac18c
gef.howie <gef.howie@b891f10e-93ca-11dd-82de-256c8af877fa>
2008-12-21 22:34:25 +0000

    New tools

---
+++ PlainOpenGL/src/com/novusradix/JavaPop/Client/Tools/VolcanoTool.java
@@ -0,0 +1,22 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.novusradix.JavaPop.Client.Tools;
+
+import java.awt.Point;
+
+/**
+ *
+ * @author mom
+ */
+public class VolcanoTool extends BaseTool {
+    
+    public void PrimaryAction(Point p) {
+    }
+
+    public void SecondaryAction(Point p) {
+         setTool(ToolType.RaiseLower);
+    }
+ 
+}
