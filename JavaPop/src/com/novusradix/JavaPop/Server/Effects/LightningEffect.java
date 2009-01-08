b5e4cbb9675859033c8100f29756b2cb3b3f7018
gef.howie <gef.howie@b891f10e-93ca-11dd-82de-256c8af877fa>
2009-01-08 21:30:21 +0000

    Working on lightning

--- JavaPop/src/com/novusradix/JavaPop/Server/Effects/LightningEffect.java
+++ JavaPop/src/com/novusradix/JavaPop/Server/Effects/LightningEffect.java
@@ -1,9 +1,13 @@
 package com.novusradix.JavaPop.Server.Effects;
 
+import com.novusradix.JavaPop.Math.Vector3;
 import com.novusradix.JavaPop.Server.Game;
 import com.novusradix.JavaPop.Server.Houses.House;
 import com.novusradix.JavaPop.Server.Peons.Peon;
 import java.awt.Point;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.util.Random;
 import javax.media.opengl.GL;
 
@@ -14,9 +18,27 @@ import javax.media.opengl.GL;
 public class LightningEffect extends Effect {
 
     Point hit;
+    transient Vector3[] strikes;
+    transient float[] times;
+    transient int nextStrike;
+    transient float lastTime;
+    transient Random r;
 
     public LightningEffect(Point target) {
         hit = target;
+    }
+
+    private void readObject(ObjectInputStream in)
+            throws IOException, ClassNotFoundException {
+        in.defaultReadObject();
+        strikes = new Vector3[5];
+        times = new float[5];
+        nextStrike = 0;
+        r = new Random();
+    }
+
+    private void writeObject(ObjectOutputStream out) throws IOException {
+        out.defaultWriteObject();
     }
 
     @Override
@@ -37,35 +59,55 @@ public class LightningEffect extends Effect {
         gl.glMatrixMode(GL.GL_MODELVIEW);
         gl.glPushMatrix();
         gl.glTranslatef(hit.x + 0.5f, hit.y + 0.5f, h);
-        float x, y, z, c;
-        Random r = new Random();
+
+        float c;
+        if (lastTime > 0) {
+            for (int n = 0; n < nextPoisson(5.0*(time - lastTime)); n++) {
+                
+                times[nextStrike] = time;
+                Vector3 v = new Vector3(r.nextFloat() - 0.5f, r.nextFloat() - 0.5f, r.nextFloat() + 0.5f);
+                strikes[nextStrike] = v;
+                nextStrike = (nextStrike + 1) % 5;
+            }
+        }
+        lastTime = time;
         gl.glDisable(GL.GL_TEXTURE_2D);
         gl.glDisable(GL.GL_LIGHTING);
         gl.glEnable(GL.GL_BLEND);
         gl.glUseProgram(0);
         gl.glBegin(GL.GL_TRIANGLES);
-        for (int n = 0; n < 4; n++) {
-            x = r.nextFloat() - 0.5f;
-            y = r.nextFloat() - 0.5f;
-            z = r.nextFloat() + 0.5f;
-            c = r.nextFloat();
+        for (int n = 0; n < 5; n++) {
 
-            gl.glColor4f(0.9f, 0.9f, 1.0f, c);
-            gl.glVertex3f(0.0f, 0.0f, 0.0f);
-            gl.glVertex3f(x, y, z);
-            gl.glVertex3f(x + 0.1f, y, z);
-            
-            gl.glVertex3f(x, y, z);
-            gl.glVertex3f(x + 0.2f, y, z - 0.2f);
-            gl.glVertex3f(x + 0.25f, y, z - 0.2f);
-            
-            gl.glVertex3f(x + 0.2f, y, z - 0.2f);
-            gl.glVertex3f(x + 0.25f, y, z - 0.2f);
-            gl.glVertex3f(x*2.0f, y*1.5f, 3.0f);
+            c = Math.max(0, 1.0f + times[n] - time);
+            if (c > 0) {
+                gl.glColor4f(0.9f, 0.9f, 1.0f, c);
+                gl.glVertex3f(0.0f, 0.0f, 0.0f);
+                gl.glVertex3f(strikes[n].x, strikes[n].y, strikes[n].z);
+                gl.glVertex3f(strikes[n].x + 0.1f, strikes[n].y, strikes[n].z);
 
+                gl.glVertex3f(strikes[n].x, strikes[n].y, strikes[n].z);
+                gl.glVertex3f(strikes[n].x + 0.2f, strikes[n].y, strikes[n].z - 0.2f);
+                gl.glVertex3f(strikes[n].x + 0.25f, strikes[n].y, strikes[n].z - 0.2f);
+
+                gl.glVertex3f(strikes[n].x + 0.2f, strikes[n].y, strikes[n].z - 0.2f);
+                gl.glVertex3f(strikes[n].x + 0.25f, strikes[n].y, strikes[n].z - 0.2f);
+                gl.glVertex3f(strikes[n].x * 2.0f, strikes[n].y * 1.5f, 3.0f);
+            }
         }
         gl.glEnd();
         gl.glPopMatrix();
+    }
 
+    public int nextPoisson(double lambda) {
+        double elambda = Math.exp(-1 * lambda);
+        double product = 1;
+        int count = 0;
+        int result = 0;
+        while (product >= elambda) {
+            product *= r.nextDouble();
+            result = count;
+            count++; // keep result one behind
+        }
+        return result;
     }
 }
