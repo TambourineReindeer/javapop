357e718432220bf8f92c48e97ca08ccb742d5290
gef.howie <gef.howie@b891f10e-93ca-11dd-82de-256c8af877fa>
2009-01-13 20:27:18 +0000

    optimising message sizes and frequency

--- JavaPop/src/com/novusradix/JavaPop/Messaging/PlayerUpdate.java
+++ JavaPop/src/com/novusradix/JavaPop/Messaging/PlayerUpdate.java
@@ -5,6 +5,11 @@
 package com.novusradix.JavaPop.Messaging;
 
 import com.novusradix.JavaPop.Server.Player.Info;
+import java.awt.Point;
+import java.io.Externalizable;
+import java.io.IOException;
+import java.io.ObjectInput;
+import java.io.ObjectOutput;
 import java.util.ArrayList;
 import java.util.Collection;
 
@@ -12,24 +17,60 @@ import java.util.Collection;
  *
  * @author mom
  */
-public class PlayerUpdate extends Message {
-
+public class PlayerUpdate extends Message implements Externalizable {
+ private static final long serialVersionUID = 1L;
+   
     private Collection<Info> info;
 
     public PlayerUpdate(Info i) {
         this.info = new ArrayList<Info>(1);
         this.info.add(i);
     }
-    
-    public PlayerUpdate(Collection<Info> is)
-    {
+
+    public PlayerUpdate(Collection<Info> is) {
         this.info = is;
     }
-    
+
     @Override
     public void execute() {
         for (Info i : info) {
             clientGame.players.get(i.id).update(i);
+        }
+    }
+    
+    public PlayerUpdate() {
+    }
+    
+    public void writeExternal(ObjectOutput o) throws IOException {
+        o.writeInt(info.size());
+        for (Info i : info) {
+            o.writeInt(i.id);
+            o.writeUTF(i.name);
+            o.writeInt(i.ankh.x);
+            o.writeInt(i.ankh.y);
+            o.writeFloat(i.colour[0]);
+            o.writeFloat(i.colour[1]);
+            o.writeFloat(i.colour[2]);
+            o.writeDouble(i.mana);
+        }
+    }
+
+    public void readExternal(ObjectInput i) throws IOException, ClassNotFoundException {
+        info = new ArrayList<Info>();
+        int n = i.readInt();
+        for (; n > 0; n--) {
+            Info inf = new Info();
+            inf.id = i.readInt();
+            inf.name = i.readUTF();
+            inf.ankh = new Point();
+            inf.ankh.x = i.readInt();
+            inf.ankh.y = i.readInt();
+            inf.colour = new float[3];
+            inf.colour[0] = i.readFloat();
+            inf.colour[1] = i.readFloat();
+            inf.colour[2] = i.readFloat();
+            inf.mana = i.readDouble();
+            info.add(inf);
         }
     }
 }
