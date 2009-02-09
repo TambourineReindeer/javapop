abebefb9b43da945103463b6df7bb3141b0f30f9
gef.howie <gef.howie@b891f10e-93ca-11dd-82de-256c8af877fa>
2009-02-09 07:00:58 +0000

    Text kerning working well

--- JavaPopTools/src/javapoptools/Fonts/KerningFrame.java
+++ JavaPopTools/src/javapoptools/Fonts/KerningFrame.java
@@ -1,11 +1,16 @@
 package javapoptools.Fonts;
 
 import com.sun.opengl.util.Animator;
+import java.awt.BorderLayout;
 import java.awt.Dimension;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.WindowEvent;
 import java.awt.event.WindowListener;
 import javax.media.opengl.GLCapabilities;
 import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
 import sun.awt.WindowClosingListener;
 
 /**
@@ -16,21 +21,32 @@ public class KerningFrame extends JFrame implements WindowListener {
 
     private KerningPanel kp;
     private Animator a;
+    private JTextField tf;
 
     KerningFrame() {
+        this.setSize(800, 800);
+        this.setLayout(new BorderLayout());
         GLCapabilities caps = new GLCapabilities();
         caps.setSampleBuffers(true);
         caps.setNumSamples(4);
+        JPanel p = new JPanel();
+        p.setPreferredSize(new Dimension(750,750));
         kp = new KerningPanel(caps);
-
-
-        kp.setPreferredSize(new Dimension(128, 128));
-        add(kp);
-
-        this.setSize(800, 800);
+        kp.setPreferredSize(new Dimension(750,750));
+        add(p, BorderLayout.CENTER);
+p.add(kp);
         a = new Animator(kp);
         a.start();
         this.addWindowListener(this);
+
+        tf = new JTextField("Test me");
+        add(tf, BorderLayout.SOUTH);
+        tf.addActionListener(new ActionListener() {
+
+            public void actionPerformed(ActionEvent e) {
+                kp.setText(tf.getText());
+            }
+        });
     }
 
     public static void main(String[] args) {
