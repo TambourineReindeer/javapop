e95dcd7dc30d0fccecfebec81a2cb3c629b3473a
gef.howie <gef.howie@b891f10e-93ca-11dd-82de-256c8af877fa>
2009-02-12 03:40:26 +0000

    added numbers and some symbols to font, kerning

--- JavaPopTools/src/javapoptools/Fonts/KerningFrame.java
+++ JavaPopTools/src/javapoptools/Fonts/KerningFrame.java
@@ -8,6 +8,8 @@ import java.awt.event.ActionListener;
 import java.awt.event.WindowEvent;
 import java.awt.event.WindowListener;
 import javax.media.opengl.GLCapabilities;
+import javax.swing.Action;
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
@@ -22,6 +24,7 @@ public class KerningFrame extends JFrame implements WindowListener {
     private KerningPanel kp;
     private Animator a;
     private JTextField tf;
+    private JTextField row,  column;
 
     KerningFrame() {
         this.setSize(800, 800);
@@ -30,23 +33,44 @@ public class KerningFrame extends JFrame implements WindowListener {
         caps.setSampleBuffers(true);
         caps.setNumSamples(4);
         JPanel p = new JPanel();
-        p.setPreferredSize(new Dimension(750,750));
+        p.setPreferredSize(new Dimension(750, 750));
         kp = new KerningPanel(caps);
-        kp.setPreferredSize(new Dimension(750,750));
+        kp.setPreferredSize(new Dimension(750, 750));
         add(p, BorderLayout.CENTER);
-p.add(kp);
+        p.add(kp);
         a = new Animator(kp);
         a.start();
         this.addWindowListener(this);
 
+        JPanel controls = new JPanel();
+        add(controls, BorderLayout.SOUTH);
         tf = new JTextField("Test me");
-        add(tf, BorderLayout.SOUTH);
+        tf.setPreferredSize(new Dimension(150, 24));
+        controls.add(tf);
         tf.addActionListener(new ActionListener() {
 
             public void actionPerformed(ActionEvent e) {
                 kp.setText(tf.getText());
             }
         });
+
+        row = new JTextField(20);
+        column = new JTextField(20);
+        row.setText("ABCDEFGHIJKLMNOPQRTUVWXYZ");
+        column.setText("ABCDEFGHIJKLMNOPQRTUVWXYZ");
+
+
+        controls.add(row);
+        controls.add(column);
+        ActionListener al = new ActionListener() {
+
+            public void actionPerformed(ActionEvent e) {
+                kp.setRowAndColumn(row.getText(), column.getText());
+            }
+        };
+        row.addActionListener(al);
+        column.addActionListener(al);
+
     }
 
     public static void main(String[] args) {
