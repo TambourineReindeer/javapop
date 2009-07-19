e59751d4594ab35ea491e43aa7dd5ceff2ccdb1a
gef.howie <gef.howie@b891f10e-93ca-11dd-82de-256c8af877fa>
2009-07-19 16:24:41 +0000

    Basic fungus working, now need to match rules to Pop II

--- JavaPop/src/com/novusradix/JavaPop/Effects/FungusEffect.java
+++ JavaPop/src/com/novusradix/JavaPop/Effects/FungusEffect.java
@@ -17,37 +17,44 @@ public class FungusEffect extends Effect {
 
     public FungusEffect(ServerGame g) {
         game = g;
-        map = new byte[g.heightMap.width + 2][g.heightMap.breadth + 2];
-        newmap = new byte[g.heightMap.width + 2][g.heightMap.breadth + 2];
+        map = new byte[g.heightMap.width + 1][g.heightMap.breadth + 1];
+        newmap = new byte[g.heightMap.width + 1][g.heightMap.breadth + 1];
     }
 
     public void addFungus(int x, int y) {
-
-        map[x + 1][y + 1] = 1;
+        if (game.heightMap.getTile(x, y).isFertile) {
+            map[x + 1][y + 1] = 1;
+            game.heightMap.setTile(x, y, Tile.FUNGUS);
+        }
     }
 
     @Override
     public void execute(ServerGame g) {
         byte[][] temp;
         int count;
-        for (int x = 1; x <= g.heightMap.width; x++) {
-            for (int y = 1; y <= g.heightMap.breadth; y++) {
-
-                count = map[x][y] + map[x - 1][y] + map[x + 1][y] + map[x][y - 1] + map[x][y + 1];
-                newmap[x][y] =   (byte) (game.heightMap.getTile(x - 1, y - 1).isFertile ? (count % 2) : 0);
-                if(newmap[x][y]!=map[x][y])
-                {
-                    if(newmap[x][y]==1)
-                        game.heightMap.setTile(x-1, y-1, Tile.FUNGUS);
-                    else
-                        game.heightMap.clearTile(x-1,y-1);
+        Tile t;
+        for (int x = 1; x < g.heightMap.width; x++) {
+            for (int y = 1; y < g.heightMap.breadth; y++) {
+                t = game.heightMap.getTile(x - 1, y - 1);
+                if (t.isFertile || t == Tile.FUNGUS) {
+                    count = map[x][y] + map[x - 1][y] + map[x + 1][y] + map[x][y - 1] + map[x][y + 1];
+                    newmap[x][y] = (byte) ((count > 0 ) ? 1 : 0);
+                } else {
+                    newmap[x][y] = 0;
+                }
+                if (newmap[x][y] != map[x][y]) {
+                    if (newmap[x][y] == 1) {
+                        game.heightMap.setTile(x - 1, y - 1, Tile.FUNGUS);
+                    } else {
+                        game.heightMap.clearTile(x - 1, y - 1);
+                    }
                 }
             }
         }
 
-        temp=newmap;
-        newmap=map;
-        map=newmap;
+        temp = newmap;
+        newmap = map;
+        map = temp;
     }
 
     @Override
